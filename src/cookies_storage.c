f06c6652bd5fc54d8fcf7e5ef12abc75d6cc1cb3
pauk.denis <pauk.denis@c38e332a-2555-0410-bd50-2794203b9896>
2009-01-10 11:47:01 +0000

    simple refactoring

---
+++ src/cookies_storage.c
@@ -0,0 +1,27 @@
+#include "cookies_storage.h"
+/*Save Cookies*/
+void cookies_storage_add(cookies_storage storage,gchar* cookie, gchar * url)
+{
+    char *posend;
+
+    g_print("\n---------------------------\nCookies=%s\n---------------\n", cookie);
+    posend = strchr(cookie, ';');
+    if (posend != NULL) {
+		char *new_cookies;
+		char *old_cookies;
+		new_cookies =
+		    calloc(strlen(*storage) + posend - cookie + 2,
+			   sizeof(gchar));
+			strcpy(new_cookies, *storage);
+			strncat(new_cookies, cookie, posend - cookie + 1);
+			g_print("NewCookies=%s\n", new_cookies);
+			old_cookies = *storage;
+			*storage = new_cookies;
+			free(old_cookies);
+    }
+}
+
+gchar* cookies_storage_get(cookies_storage storage, gchar * url)
+{
+	return *storage;
+}
