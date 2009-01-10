cf425e78b9710e1adeaf47a2f903d58a9200121f
Denis Pauk <pauk.denis@gmail.com>
2009-01-10 21:39:00 +0000

    little refactoting: no improwements

--- src/loaders.h
+++ src/loaders.h
@@ -2,8 +2,12 @@
 #define LOADERS_H
 #include <config.h>
 #include <string.h>
+#include "cookies_storage.h"
 #include <gtkhtml/gtkhtml.h>
 /*#define DebugLoaders*/
-gchar * get_data_content(const gchar * action, gsize * length, gchar ** contentType);
-gchar * get_default_content(const gchar * action, gsize * length, gchar * contentType);
+/*gchar * get_http_content	(const gchar * action, gsize * length, gchar ** contentType,
+								gchar* method, gchar* encoding, gchar** curr_base,
+								cookies_storage cookies_save, SoupSession* session);*/
+gchar * get_data_content	(const gchar * action, gsize * length, gchar ** contentType);
+gchar * get_default_content	(const gchar * action, gsize * length, gchar ** contentType);
 #endif
