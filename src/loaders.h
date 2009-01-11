650e0d0fbc591c906c06985f3e53ff666f419981
Denis Pauk <pauk.denis@gmail.com>
2009-01-11 07:08:26 +0000

    new version cookies_storage

--- src/loaders.h
+++ src/loaders.h
@@ -1,13 +1,11 @@
 #ifndef LOADERS_H
 #define LOADERS_H
-#include <config.h>
-#include <string.h>
 #include "cookies_storage.h"
-#include <gtkhtml/gtkhtml.h>
+#include <libsoup/soup.h>
 /*#define DebugLoaders*/
-/*gchar * get_http_content	(const gchar * action, gsize * length, gchar ** contentType,
-								gchar* method, gchar* encoding, gchar** curr_base,
-								cookies_storage cookies_save, SoupSession* session);*/
+gchar * get_http_content(const gchar * action, gsize * length, gchar ** contentType,
+				gchar* method, gchar* encoding, gchar** curr_base,
+				cookies_storage* cookies_save, SoupSession* session);
 gchar * get_data_content	(const gchar * action, gsize * length, gchar ** contentType);
 gchar * get_default_content	(const gchar * action, gsize * length, gchar ** contentType);
 #endif
