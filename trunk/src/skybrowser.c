ed69a47f1ac8e4194696d09a1abbfd57d8e66133
pauk.denis <pauk.denis@c38e332a-2555-0410-bd50-2794203b9896>
2009-03-06 20:34:10 +0000

    ported all public features from gtkhtml

--- trunk/src/skybrowser.c
+++ trunk/src/skybrowser.c
@@ -21,6 +21,7 @@
 #include <stdlib.h>
 #include <glib.h>
 #include <gtk/gtk.h>
+#include "htmlengineinterface.h"
 
 /*#define DebugVariable*/
 
@@ -33,16 +34,48 @@ on_exit_window(GtkWidget * window, gpointer data)
 }
 
 static void
+print_action (GtkWidget *widget, gpointer data)
+{
+  html_engine_interface_print(data);
+}
+
+static void
 quit_action (GtkWidget *widget, gpointer data)
 {
   html_engine_interface_stop_query(data);
   gtk_main_quit();
 }
 
+static void
+forward_action(GtkWidget *widget, gpointer data)
+{
+	struct All_variable *variable = (struct All_variable *) data;
+	gchar* text = g_strdup(
+						gtk_entry_get_text(GTK_ENTRY(variable->textentry))
+				);
+	html_engine_intreface_go(variable, text);
+}
+
+static void
+back_action(GtkWidget *widget, gpointer data)
+{
+	g_print("not implemented yet");
+}
+
 /*standart main function*/
 int
 main(int argc, char **argv)
 {
+	GtkWidget *scrolled_window;
+	GtkWidget *textentry;
+	GtkWidget *app;
+	GtkWidget *table;
+	GtkWidget *action_table;
+	GtkWidget *button_quit;
+	GtkWidget *button_forward;
+	GtkWidget *button_back;
+	GtkWidget *button_print;
+	struct All_variable *variable;
     /* initialization support i18n */
     gtk_set_locale();
 
@@ -50,28 +83,30 @@ main(int argc, char **argv)
     gtk_init(&argc, &argv);
 
     /*main window*/
-    GtkWidget *app = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    app = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_default_size(GTK_WINDOW(app), 640, 480);
 	
-	struct All_variable *variable = html_engine_intreface_construct();	
+	variable = html_engine_intreface_construct();	
     g_signal_connect(app, "delete-event",
 		     G_CALLBACK(on_exit_window), variable);
 			 
 	/*main table*/
-	GtkWidget *table = gtk_table_new (1, 2, FALSE);
+	table = gtk_table_new (1, 2, FALSE);
 	gtk_container_add (GTK_CONTAINER (app), table);
 	/*action group*/
-	GtkWidget *action_table = gtk_table_new (4, 1, FALSE);
+	action_table = gtk_table_new (5, 1, FALSE);
 	/*back*/
-	GtkWidget *buttonback = gtk_button_new_from_stock(GTK_STOCK_GO_BACK);
+	button_back = gtk_button_new_from_stock(GTK_STOCK_GO_BACK);
 	gtk_table_attach (GTK_TABLE (action_table),
-                        buttonback,
+                        button_back,
                         /* X direction */       /* Y direction */
                         0, 1,                   0, 1,
                         GTK_SHRINK,  			GTK_SHRINK,
                         0,                      0);
+	g_signal_connect (G_OBJECT (button_back), "clicked",
+						G_CALLBACK (back_action), variable);
 	/*entry*/
-	GtkWidget *textentry = gtk_entry_new ();
+	textentry = gtk_entry_new ();
 	gtk_table_attach (GTK_TABLE (action_table),
                         textentry,
                         /* X direction */       /* Y direction */
@@ -79,22 +114,34 @@ main(int argc, char **argv)
                         GTK_EXPAND | GTK_FILL,  GTK_EXPAND | GTK_FILL,
                         0,                      0);
 	/*forward*/
-	GtkWidget *buttonforward = gtk_button_new_from_stock(GTK_STOCK_GO_FORWARD);
+	button_forward = gtk_button_new_from_stock(GTK_STOCK_GO_FORWARD);
 	gtk_table_attach (GTK_TABLE (action_table),
-                        buttonforward,
+                        button_forward,
                         /* X direction */       /* Y direction */
                         2, 3,                   0, 1,
                         GTK_SHRINK,  			GTK_SHRINK,
                         0,                      0);
-	/*quit*/
-	GtkWidget *buttonquit = gtk_button_new_from_stock(GTK_STOCK_QUIT);
+	g_signal_connect (G_OBJECT (button_forward), "clicked",
+						G_CALLBACK (forward_action), variable);
+	/*print*/
+	button_print = gtk_button_new_from_stock(GTK_STOCK_PRINT);
 	gtk_table_attach (GTK_TABLE (action_table),
-                        buttonquit,
+                        button_print,
                         /* X direction */       /* Y direction */
                         3, 4,                   0, 1,
                         GTK_SHRINK,  			GTK_SHRINK,
                         0,                      0);
-	g_signal_connect (G_OBJECT (buttonquit), "clicked",
+	g_signal_connect (G_OBJECT (button_print), "clicked",
+						G_CALLBACK (print_action), variable);
+	/*quit*/
+	button_quit = gtk_button_new_from_stock(GTK_STOCK_QUIT);
+	gtk_table_attach (GTK_TABLE (action_table),
+                        button_quit,
+                        /* X direction */       /* Y direction */
+                        4, 5,                   0, 1,
+                        GTK_SHRINK,  			GTK_SHRINK,
+                        0,                      0);
+	g_signal_connect (G_OBJECT (button_quit), "clicked",
 						G_CALLBACK (quit_action), variable);
 	/*add action table to main table*/
 	gtk_table_attach (GTK_TABLE (table),
@@ -104,7 +151,7 @@ main(int argc, char **argv)
                         GTK_EXPAND | GTK_FILL,	GTK_SHRINK,
                         0,                      0);
     /*scrolled content window*/
-	GtkWidget *scrolled_window = gtk_scrolled_window_new(NULL, NULL);
+	scrolled_window = gtk_scrolled_window_new(NULL, NULL);
     gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW
 				   (scrolled_window),
 				   GTK_POLICY_AUTOMATIC,
@@ -120,7 +167,9 @@ main(int argc, char **argv)
 	/* add html render*/
 
     gtk_container_add(GTK_CONTAINER(scrolled_window),
-		      html_engine_intreface_init( variable, app));
+			GTK_WIDGET(
+				html_engine_interface_init( variable, app, textentry)
+			));
 	/*show new window*/
     gtk_widget_show_all(app);
 	
